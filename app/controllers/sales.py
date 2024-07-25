c4ea8f15e9e74e336f8d8560560a812996b6ec20
millergarcia <millergarciaq@gmail.com>
2024-07-24 22:39:52 -0500

    added api to calculate total and average for store by id

--- app/controllers/sales.py
+++ app/controllers/sales.py
@@ -1,7 +1,7 @@
 from typing import Optional
 
 from app.controllers.datamart import DatamartController
-from app.models.sales import SalesModel
+from app.models.sales import SalesModel, SalesAverageModel
 from app.schemas.sales_query_params import SalesQueryParams
 
 
@@ -38,3 +38,28 @@ class SalesController:
     def get_sales_by_key_store(self, params: SalesQueryParams) -> Optional[SalesModel]:
         return self.get_sales_by_key('KeyStore', 'Stores', 'StoreName', params)
 
+    def get_sales_average_by_key(self, key_type: str, data_key: str, data_name: str, params: SalesQueryParams) -> Optional[SalesAverageModel]:
+
+        filtered_data = self.datamart.dataframe[
+            (self.datamart.dataframe[key_type] == params.key_value) &
+            (self.datamart.dataframe['KeyDate'] >= params.start_date) &
+            (self.datamart.dataframe['KeyDate'] <= params.end_date)
+            ]
+
+        if filtered_data.empty:
+            return None
+
+        total_sales = filtered_data['Amount'].sum()
+        average_sales = filtered_data['Amount'].mean()
+
+        sales = SalesAverageModel(
+            total=int(total_sales),
+            average=float(average_sales),
+            name=filtered_data.iloc[0][data_key][data_name],
+            start_date=params.start_date,
+            end_date=params.end_date
+        )
+        return sales
+    
+    def get_sales_average_by_key_store(self, params: SalesQueryParams) -> Optional[SalesAverageModel]:
+        return self.get_sales_average_by_key('KeyStore', 'Stores', 'StoreName', params)
--- app/routers/sales_router.py
+++ app/routers/sales_router.py
@@ -24,6 +24,7 @@ def sales_by_employee(
         return data
     return HTTPException(status_code=404, detail='Item not found')
 
+
 @router.get("/sales/by-product/{product_id}")
 def sales_by_product(
     product_id: str,
@@ -38,6 +39,7 @@ def sales_by_product(
         return data
     return HTTPException(status_code=404, detail='Item not found')
 
+
 @router.get("/sales/by-store/{store_id}")
 def sales_by_store(
     store_id: str,
@@ -48,6 +50,21 @@ def sales_by_store(
     sales_controller = SalesController(datamart)
     params = SalesQueryParams(start_date=start_date, end_date=end_date, key_value=store_id)
     data = sales_controller.get_sales_by_key_store(params)
+    if data:
+        return data
+    return HTTPException(status_code=404, detail='Item not found')
+
+
+@router.get("/total-and-average-sales/by-store/{store_id}")
+def total_and_average_sales_by_store(
+    store_id: str,
+    start_date: date = Query(..., description="Start date of the period"),
+    end_date: date = Query(..., description="End date of the period"),
+    datamart: DatamartController = Depends(get_datamart_controller)
+):
+    sales_controller = SalesController(datamart)
+    params = SalesQueryParams(start_date=start_date, end_date=end_date, key_value=store_id)
+    data = sales_controller.get_sales_average_by_key_store(params)
     if data:
         return data
     return HTTPException(status_code=404, detail='Item not found')
