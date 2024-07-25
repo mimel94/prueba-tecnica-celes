dbf4f4a8537bd63c38f516f4c700067cb9bd46c0
millergarcia <millergarciaq@gmail.com>
2024-07-25 13:44:37 -0500

    test for api search by key store

--- tests/units/controllers/test_sales_controller.py
+++ tests/units/controllers/test_sales_controller.py
@@ -7,8 +7,9 @@ from app.schemas.sales_query_params import SalesQueryParams
 
 
 class TestSalesController:
-    def test_given_valid_sales_params_when_query_sales_by_employee_then_return_correct_sales_data(self, mocker, data_fixture):
-        mocker.patch.object(DatamartController, 'load_data', return_value=data_fixture)
+    def test_given_valid_sales_params_when_query_sales_by_employee_then_return_correct_sales_data(self, mocker,
+                                                                                                  data_fixture_employee):
+        mocker.patch.object(DatamartController, 'load_data', return_value=data_fixture_employee)
         data_controller = SalesController(datamart_model=DatamartController())
         response = data_controller.get_sales_by_key_employee(params=SalesQueryParams(start_date=date(2023, 11, 1),
                                                                           end_date=date(2024, 1, 31),
@@ -18,8 +19,9 @@ class TestSalesController:
         assert response.end_date == date(2024,1,31)
         assert response.name == 'ROMERO MEZA FREDDY JESUS'
 
-    def test_given_invalid_sales_params_when_query_sales_by_employee_then_return_none_sales_data(self, mocker, data_fixture):
-        mocker.patch.object(DatamartController, 'load_data', return_value=data_fixture)
+    def test_given_invalid_sales_params_when_query_sales_by_employee_then_return_none_sales_data(self, mocker,
+                                                                                                 data_fixture_employee):
+        mocker.patch.object(DatamartController, 'load_data', return_value=data_fixture_employee)
         data_controller = SalesController(datamart_model=DatamartController())
         response = data_controller.get_sales_by_key_employee(params=SalesQueryParams(start_date=date(2023, 11, 1),
                                                                                      end_date=date(2024, 1, 31),
@@ -27,3 +29,24 @@ class TestSalesController:
         assert response is None
 
 
+    def test_given_valid_sales_params_when_query_sales_by_store_then_return_correct_sales_data(self, mocker,
+                                                                                               data_fixture_store):
+        mocker.patch.object(DatamartController, 'load_data', return_value=data_fixture_store)
+        data_controller = SalesController(datamart_model=DatamartController())
+        response = data_controller.get_sales_by_key_store(params=SalesQueryParams(start_date=date(2023, 11, 1),
+                                                                          end_date=date(2024, 1, 31),
+                                                                          key_value='1|023'))
+        assert response.total_sales == 1512
+        assert response.start_date == date(2023,11,1)
+        assert response.end_date == date(2024,1,31)
+        assert response.name == 'UNICO BQLLA'
+
+
+    def test_given_invalid_sales_params_when_query_sales_by_store_then_return_correct_sales_data(self, mocker,
+                                                                                               data_fixture_store):
+        mocker.patch.object(DatamartController, 'load_data', return_value=data_fixture_store)
+        data_controller = SalesController(datamart_model=DatamartController())
+        response = data_controller.get_sales_by_key_store(params=SalesQueryParams(start_date=date(2023, 11, 1),
+                                                                          end_date=date(2024, 1, 31),
+                                                                          key_value='1|021'))
+        assert response is None
