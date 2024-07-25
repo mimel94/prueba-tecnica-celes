ecc6cdb8af05a3a731db9747c78f598f488cf71b
millergarcia <millergarciaq@gmail.com>
2024-07-25 13:20:43 -0500

    create test with mocks and fake data for employee sales

--- settings/base.py
+++ settings/base.py
@@ -1,8 +1,6 @@
 from starlette.config import Config
 
 
-
 config = Config('.env')
-DATA_DIRECTORY = config('DATA_DIRECTORY')
-SECRET_KEY = config('SECRET_KEY')
-
+DATA_DIRECTORY = config('DATA_DIRECTORY', default='')
+SECRET_KEY = config('SECRET_KEY', default='')
---
+++ tests/units/controllers/test_sales_controller.py
@@ -0,0 +1,29 @@
+from datetime import date
+from unittest.mock import patch
+
+from app.controllers.datamart import DatamartController
+from app.controllers.sales import SalesController
+from app.schemas.sales_query_params import SalesQueryParams
+
+
+class TestSalesController:
+    def test_given_valid_sales_params_when_query_sales_by_employee_then_return_correct_sales_data(self, mocker, data_fixture):
+        mocker.patch.object(DatamartController, 'load_data', return_value=data_fixture)
+        data_controller = SalesController(datamart_model=DatamartController())
+        response = data_controller.get_sales_by_key_employee(params=SalesQueryParams(start_date=date(2023, 11, 1),
+                                                                          end_date=date(2024, 1, 31),
+                                                                          key_value='1|569'))
+        assert response.total_sales == 113
+        assert response.start_date == date(2023,11,1)
+        assert response.end_date == date(2024,1,31)
+        assert response.name == 'ROMERO MEZA FREDDY JESUS'
+
+    def test_given_invalid_sales_params_when_query_sales_by_employee_then_return_none_sales_data(self, mocker, data_fixture):
+        mocker.patch.object(DatamartController, 'load_data', return_value=data_fixture)
+        data_controller = SalesController(datamart_model=DatamartController())
+        response = data_controller.get_sales_by_key_employee(params=SalesQueryParams(start_date=date(2023, 11, 1),
+                                                                                     end_date=date(2024, 1, 31),
+                                                                                     key_value='1|564'))
+        assert response is None
+
+
